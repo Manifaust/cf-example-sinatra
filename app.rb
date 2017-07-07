f5cf446b1ebba359249621806e9ff2ee4c38e8e2
Tony Wong <twong@pivotal.io>
2017-07-07 13:27:37 -0400

    Show user agent and POST data

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/reloader'
+require 'net/http'
+require 'uri'
 
 configure do
   set :views,         File.dirname(__FILE__) + '/views'
@@ -8,9 +10,23 @@ configure do
 end
 
 get '/' do
-  @host = ENV['CF_INSTANCE_IP'] || "192.168.0.0"
-  @port = ENV['CF_INSTANCE_PORT'] || "9292"
-  @index = ENV['CF_INSTANCE_INDEX'] || "0"
+  @host = ENV['CF_INSTANCE_IP'] || '192.168.0.0'
+  @port = ENV['CF_INSTANCE_PORT'] || '9292'
+  @index = ENV['CF_INSTANCE_INDEX'] || '0'
+  @agent = request.user_agent
+
+
+  uri = URI.parse(ENV['EIP_ENDPOINT'])
+
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  header = { 'Content-Type' => 'text/plain' }
+  net_request = Net::HTTP::Post.new(uri.request_uri, header)
+  net_request.body = @agent
+
+  @response_body = http.request(net_request).body
+
+  # redirect "#{@response_body}"
 
   erb :index
 end
